4e8844715baea4c8dccc28964db65daadea32edd
Al-waleed shihadeh <wshihadeh.dev@gmail.com>
2019-12-14 19:09:21 +0100

    Integrate portus

--- capistrano/config/deploy/local.rb
+++ capistrano/config/deploy/local.rb
@@ -6,10 +6,15 @@ server '127.0.0.1', user: 'shihadeh',
                            nginx
                            ldap
                            registry
+                           portus
                            swarm_manager
                          ]
 
-set :shared, network: fetch(:network)
+set :shared,
+     network: fetch(:network),
+     registry_domain: 'hub.shihadeh.intern',
+     registry_ip:     '192.168.178.26'
+
 set (:deploy_to) { '/Users/shihadeh/personal/swarm_orcs_deployments/platform_services' }
 set (:config_path) { "#{fetch(:deploy_to)}/configs" }
 
@@ -22,7 +27,7 @@ set :mysql,
 set :nginx,
     stack_name: 'nginx',
     nginx_docker_image: 'wshihadeh/nginx',
-    nginx_docker_image_tag: 'latest',
+    nginx_docker_image_tag: 'v2',
     nginx_replica_count: 1,
     port_http: '80',
     port_https: '443'
@@ -46,3 +51,37 @@ set :registry,
     registry_storage_filesystem_rootdirectory: '/var/lib/registry',
     registry_volume: "#{fetch(:deploy_to)}/registry",
     config_path: fetch(:config_path)
+
+set :portus,
+    stack_name: 'portus',
+    portus_machine_fqdn_value: 'hub.shihadeh.intern',
+    portus_db_host: 'mysql_server',
+    portus_db_database: 'portus_production',
+    portus_db_username: 'root',
+    portus_db_password: 'dummy',
+    portus_db_pool: '5',
+    config_prefix: 'PORTUS',
+    config_path: fetch(:config_path),
+    portus_secret_key_base: '41080f4bf7415093858fdc871b873a668a5e2eb71d07bee3c43524fe9b6a63a2605da513abf7e8a6c56504586cf1a681f4eeb3f7dbea50ebb6da51bd5be1791b',
+    portus_key_path: '/certificates/hub.shihadeh.intern.key',
+    portus_password: 'test123',
+    portus_check_ssl_usage_enabled: 'false',
+    rails_serve_static_files: 'true',
+    portus_ldap_enabled: 'true',
+    portus_ldap_hostname: 'ldap_server',
+    portus_ldap_port: '389',
+    portus_ldap_uid: 'givenName',
+    portus_ldap_base: 'dc=shihadeh,dc=intern',
+    portus_ldap_filter: '',
+    portus_ldap_authentication_enabled: 'true',
+    portus_ldap_authentication_bind_dn: 'cn=admin,dc=shihadeh,dc=intern',
+    portus_ldap_authentication_password: 'test1234',
+    portus_ldap_encryption_method: 'plain',
+    portus_delete_enabled: 'true',
+    portus_delete_contributors: 'true',
+    portus_delete_garbage_collector_enabled: 'false',
+    portus_delete_garbage_collector_older_than: '60',
+    portus_delete_garbage_collector_keep_latest: '5',
+    portus_delete_garbage_collector_tag: 'featuer*',
+    portus_pagination_limit: '10',
+    portus_pagination_before_after: '2'
